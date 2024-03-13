7572b072a44b1a77765852b059c4fbcc2afd4894
Pedro <pedro.gilo@edge.com.br>
2024-03-13 14:18:52 -0300

    Atualiza scripts de install e run.

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
-javac -d out -cp "lib\gson-2.8.2.jar;lib\itextpdf-5.5.9.jar" application\Main.java utilities\*.java adapter\*.java userInterface\*.java dataInterface\*.java application\*.java
+javac -d out -cp "lib\gson-2.8.2.jar;lib\itextpdf-5.5.9.jar;lib\lanterna-3.1.2.jar" application\Main.java utilities\*.java adapter\*.java userInterface\*.java dataInterface\*.java application\*.java
 jar -cvfe ClientConnect.jar application.Main -C out .
--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-javaw -cp "ClientConnect.jar;lib\gson-2.8.2.jar;lib\itextpdf-5.5.9.jar" application.Main
+javaw -cp "ClientConnect.jar;lib\gson-2.8.2.jar;lib\itextpdf-5.5.9.jar;lib\lanterna-3.1.2.jar" application.Main
